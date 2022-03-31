11125a95c5af9fa7bc943b95b7dfff09ca40b2b4
Grace Gamble <grace.gamble@lifeomic.com>
2022-03-31 12:24:28 -0400

    separated header and footer

---
+++ app/public/wp-content/themes/learnwp/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body <?php body_class(); ?>>
+    <header>
+      <section class="top-bar">
+        <div class="social-media-icons"></div>
+        <div class="search"></div>
+      </section>
+      <section class="menu-area">
+        <section class="logo"></section>
+        <nav class="main-menu"></nav>
+      </section>
+    </header>
--- app/public/wp-content/themes/learnwp/index.php
+++ app/public/wp-content/themes/learnwp/index.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <p>Hello world</p>
-  </body>
-</html>
+<?php get_header(); ?>
+  <div class="content-area">
+    <main>
+      <section class="slide"></section>
+      <section class="services"></section>
+      <section class="middle-area">
+        <aside class="sidebar"></aside>
+        <div class="news"></div>
+      </section>
+      <section class="map"></section>
+    </main>
+  </div>
+<?php get_footer(); ?>
