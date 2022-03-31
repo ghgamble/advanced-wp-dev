de70b43ac591ac459035684620017f014e7f33f5
Grace Gamble <grace.gamble@lifeomic.com>
2022-03-31 14:18:50 -0400

    added blog, and separate templates, style and scripts

---
+++ app/public/wp-content/themes/learnwp/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Including stylesheets and script files
+function load_scripts() {
+  wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.1.3', true);
+  wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.3', 'all');
+  wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'load_scripts');
+
+// Create menus for theme, otherwise wouldn't display in admin
+register_nav_menus (
+  array (
+    'main_menu' => 'Main Menu',
+    'footer_menu' => 'Footer Menu'
+  )
+);
--- app/public/wp-content/themes/learnwp/page-home.php
+++ app/public/wp-content/themes/learnwp/page-home.php
@@ -16,23 +16,7 @@
           <div class="row">
             <aside class="sidebar col-md-3">Sidebar</aside>
             <div class="news col-md-9">
-              <?php
-                if(have_posts()):
-                  while(have_posts()): the_post();
-                ?>
-                <article>
-                  <h2><?php the_title(); ?></h2>
-                  <p>Posted on <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                  <p>Categories: <?php the_category(' '); ?></p>
-                  <p><?php the_tags('Tags: ', ', '); ?></p>
-                  <p><?php the_content(); ?></p>
-                </article>
-                <?php
-                endwhile;
-                else:
-                ?>
-                <p>There's nothing yet to be displayed!</p>
-              <?php endif; ?>
+              <p>This will be the news area of the home page</p>
             </div>
           </div>
         </div>
