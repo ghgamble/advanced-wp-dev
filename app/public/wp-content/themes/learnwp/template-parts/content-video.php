8e50be5659ad1d4be5a7a14e66998970a6e8c613
Grace Gamble <grace.gamble@lifeomic.com>
2022-04-05 11:57:42 -0400

    adjust permalink for posts

--- app/public/wp-content/themes/learnwp/template-parts/content-video.php
+++ app/public/wp-content/themes/learnwp/template-parts/content-video.php
@@ -1,10 +1,10 @@
 <article <?php post_class(); ?>>
-  <h2><?php the_title(); ?></h2>
-  <?php the_post_thumbnail(array(275, 275)); ?>
+  <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+  <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(275, 275)); ?></a>
   <div class="meta-info">
     <p>Posted on <?php echo get_the_date(); ?></p>
     <p>Categories: <?php the_category(' '); ?></p>
-    <p><?php the_tags('Tags: ', ', '); ?></p>    
+    <p><?php the_tags('Tags: ', ', '); ?></p>
   </div>
   <p><?php the_content(); ?></p>
 </article>
